67463832639ea54a491e8a6fd702d60235f0d31e
Charles Essien <cessien88@gmail.com>
2020-02-26 20:22:33 -0500

    kill nrf

--- reciever.cpp
+++ reciever.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "nrf24/RF24.h"
+#include "RF24/RF24.h"
+
 
 using namespace std;
 
@@ -13,38 +14,38 @@ int main() {
 
   RF24 radio(PIN_CE, PIN_CSN);
 
-  radio.begin();
-  radio.setChannel(5);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.setDataRate(RF24_1MBPS);
-  radio.enableDynamicPayloads();
+  // radio.begin();
+  // radio.setChannel(5);
+  // radio.setPALevel(RF24_PA_HIGH);
+  // radio.setDataRate(RF24_1MBPS);
+  // radio.enableDynamicPayloads();
 
-  radio.openReadingPipe(0, 0x7878787878LL);
+  // radio.openReadingPipe(0, 0x7878787878LL);
 
-  radio.printDetails();
+  // radio.printDetails();
 
-  radio.startListening(); 
+  // radio.startListening(); 
 
-  cout << "Start listening..." << endl;
+  // cout << "Start listening..." << endl;
 
-  while (true) {
+  // while (true) {
 
-    if (radio.available(&pipeNumber)) {
+  //   if (radio.available(&pipeNumber)) {
 
-      payloadSize = radio.getDynamicPayloadSize();
-      char payload[payloadSize];
-      string receivedData;
-      radio.read(&payload, sizeof(payload));
+  //     payloadSize = radio.getDynamicPayloadSize();
+  //     char payload[payloadSize];
+  //     string receivedData;
+  //     radio.read(&payload, sizeof(payload));
 
-      for (uint8_t i = 0; i < payloadSize; i++) {
-          receivedData += payload[i];
-      }
-      cout << "Pipe : " << (int) pipeNumber << " ";
-      cout << "Size : " << (int) payloadSize << " ";
-      cout << "Data : " << receivedData << endl;
+  //     for (uint8_t i = 0; i < payloadSize; i++) {
+  //         receivedData += payload[i];
+  //     }
+  //     cout << "Pipe : " << (int) pipeNumber << " ";
+  //     cout << "Size : " << (int) payloadSize << " ";
+  //     cout << "Data : " << receivedData << endl;
 
-      delay(100);
-    }
-  }
+  //     delay(100);
+  //   }
+  // }
   return 0;
 }
