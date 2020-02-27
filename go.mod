8753517edc922344280a8a59933bea419ab4f5af
Charles Essien <cessien88@gmail.com>
2020-02-26 21:44:35 -0500

    switching to remote development/ compilation

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	fyne.io/fyne v1.2.2
+	github.com/Draal/gorf24 v0.0.0-20200119003919-15e907d525d3
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	rf24 "github.com/Draal/gorf24"
 	"github.com/cessien/cubbybot/gui"
 )
 
 func main() {
 	gui.InitializeHome()
+
+	radio := rf24.New("/dev/spidev0.0", 8000000, 25)
+	defer r.Delete()
+
+	radio.Begin()
+	radio.SetRetries(15, 15)
+
 }
