f51d77e5e08649012f44a92cd22528c52747f78f
Charles Essien <cessien88@gmail.com>
2020-02-29 03:33:31 +0000

    first time writing to rf24/spi!

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"encoding/binary"
 	rf24 "github.com/Draal/gorf24"
-	"github.com/cessien/cubbybot/gui"
+	//"github.com/cessien/cubbybot/gui"
 )
 
 func main() {
-	gui.InitializeHome()
+	//gui.InitializeHome()
 
-	radio := rf24.New("/dev/spidev0.0", 8000000, 25)
-	defer r.Delete()
+	var pipe uint64 = 0xF0F0F0F0E1
+
+	radio := rf24.New(17, 25, 8000000)
+	defer radio.Delete()
 
 	radio.Begin()
 	radio.SetRetries(15, 15)
+	radio.SetAutoAck(true)
+	radio.OpenReadingPipe(1, pipe)
+	radio.StartListening()
+	radio.PrintDetails()
+
+	for {
+		if radio.Available() {
+			data := radio.Read(4)
+			fmt.Printf("data: %v\n", data)
+
+			payload := binary.LittleEndian.Uint32(data)
+			fmt.Printf("Received %v\n", payload)
+		}
+	}
 
 }
